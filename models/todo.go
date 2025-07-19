7f9fc9ad10e972a7bfb46a87acd3e3ca5cb59602
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2025-07-19 07:41:39 +0530

    changed printing format of tasks

--- models/todo.go
+++ models/todo.go
@@ -1,7 +1,13 @@
 package models
 
+import "fmt"
+
 type Todo struct {
 	Id     int
 	Task   string
 	Status bool
+}
+
+func (t Todo) String() string {
+	return fmt.Sprintf("Task #%d %s %t", t.Id, t.Task, t.Status)
 }
